83905b758c80aac9a80841ff5c77b12b10b4d059
Danilo Freire <danilo.v.estudo@gmail.com>
2023-10-02 19:35:38 -0300

    adicionando fallback de transferência não suportada

--- app/controllers/transferences_controller.rb
+++ app/controllers/transferences_controller.rb
@@ -1,30 +1,28 @@
 class TransferencesController < ApplicationController
 
   def create
+    puts "algo"
     transfer_data = JSON.parse(request.body.read)
     origin_account = transfer_data['origin_account']
     target_account = transfer_data['target_account']
     value = transfer_data['value'].to_f
-    type = transfer_data['type']
+    transference_type = transfer_data['transference_type']
 
     #@target_account_exist = AccountApiService.check_target_account(target_account)
-    @can_transfer = AccountApiService.check_balance(origin_account, value)
+    can_transfer = true #AccountApiService.check_balance(origin_account, value)
 
     if can_transfer
-      if type == 1
+      if transference_type == 1
         render json: { message: 'Transferência por PIX realizada com sucesso' }
-      elsif type == 2
+      elsif transference_type == 2
         render json: { message: 'Transferência por TED realizada com sucesso' }
-      elsif type == 3
+      elsif transference_type == 3
         render json: { message: 'Transferência por DOC realizada com sucesso' }
+      else
+        render json: { error: 'Tipo de transferência não suportada' }, status: :unprocessable_entity
       end
-    
     else 
       render json: { error: 'Saldo insuficiente para realizar a transferência' }, status: :unprocessable_entity
-    
     end
-
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Option 1 - Allow all controller actions
   #resources :transferences
   # Option 2 - Allow specific controller actions
-  resources :transferences, only: [:index, :create, :destroy]
+  #resources :transferences, only: [:index, :create, :destroy]
   # Option 3 - Define specific controller action
   #get '/transferences', to: 'transferences#index'
 
